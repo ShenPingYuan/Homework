09a04724c16a62403cd24edefef78be356e408e1
沈平元 <2439739932@qq.com>
2021-03-19 14:19:40 +0800

    搭建基础架构

---
+++ src/Homework.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions option) : base(option)
+        {
+        }
+    }
+}
